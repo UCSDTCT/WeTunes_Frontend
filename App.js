8400175097dba10b9017aba7ac0ab43b5a30cd98
David Hacker <dmhacker.cs@gmail.com>
2018-05-24 21:57:23 -0700

    Add basic authentication

--- App.js
+++ App.js
@@ -1,21 +1,18 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator } from 'react-navigation';
+
+import { LoginScreen } from './screens/HomeScreen';
+
+const RootStack = createStackNavigator({
+  Login: {
+    screen: LoginScreen,
+    title: "Login"
+  }
+});
 
 export default class App extends React.Component {
   render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    );
+    return <RootStack/>;
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
