f52f71a2c3c2a32d98fb6e428e84e30a13b72c16
David Hacker <dmhacker@protonmail.com>
2018-06-01 18:06:03 -0700

    Login/landing page prototype

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,6 +1,8 @@
 import React from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
 import { AuthSession } from 'expo';
+
+import Button from 'react-native-button';
 
 const SPOTIFY_APP_ID = '6a2f92555e304d5f83673df87483dfd5'; 
 
@@ -12,15 +14,27 @@ class LoginScreen extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Button title="Open Spotify Auth" onPress={this._handlePressAsync} />
-        {this.state.result ? (
-          <Text>{JSON.stringify(this.state.result)}</Text>
-        ) : null}
+        <View style={styles.body}>
+          <Text style={styles.header}>WeTunes</Text>
+          <Text style={styles.subheader}>Tired of one person's terrible music choices?</Text>
+          <Text style={styles.subheader}>This app may be of use to you then.</Text>
+          <Text style={styles.subheader}>Login with your Spotify account to begin.</Text>
+        </View>
+        <View style={styles.footer}>
+          <Button 
+            style={styles.spotify}
+            onPress={this._handleSpotifyAuth}>
+            <Image source={require('../assets/spotify-logo.jpg')} style={{width: 100, height: 100}}/>Connect
+          </Button>
+          {this.state.result ? (
+            <Text>{JSON.stringify(this.state.result)}</Text>
+          ) : null}
+        </View>
       </View>
     );
   }
 
-  _handlePressAsync = async () => {
+  _handleSpotifyAuth = async () => {
     let redirectUrl = AuthSession.getRedirectUrl();
     let scopes = 'user-read-private user-read-email';
     let result = await AuthSession.startAsync({
@@ -40,8 +54,30 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
+    justifyContent: 'center'
+  },
+  body: {
+    flex: 1,
+    alignItems: 'center',
     justifyContent: 'center',
   },
+  footer: {
+    marginBottom: 50
+  },
+  header: {
+    fontSize: 80
+  },
+  subheader: {
+    marginTop: 20,
+
+  },
+  spotify: {
+    color: 'white', 
+    backgroundColor: '#23CF5F', 
+    fontSize: 48,
+    padding: 21,
+    overflow: 'hidden'
+  }
 });
 
 export { LoginScreen };
